be0523d6683d8ae5e6147df11878a740c0411a03
GhadyAbuRaya <vxytjghx7d@privaterelay.appleid.com>
2025-11-04 15:52:23 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,12 +1,15 @@
 
 
 def find_max_number(num1, num2, num3):
-    if num1 > num2 and num1 > num3:
-        return num1
-    elif num2 > num1 and num2 > num3:
-        return num2
-    else:
-        return num3
+  num1 = float(input("Enter first number: "))
+  num2 = float(input("Enter second number: "))
+  num3 = float(input("Enter third number: "))
+  if num1 > num2 and num1 > num3:
+      return num1
+  elif num2 > num1 and num2 > num3:
+      return num2
+  else:
+      return num3
 
 def find_mean(num1, num2, num3):
     return (num1 + num2 + num3) / 3
